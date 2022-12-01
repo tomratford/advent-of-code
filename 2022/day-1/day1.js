af469ada470614fdb946d7ff939371f6b1879d13
Tom Ratford <tom.ratford@veramed.co.uk>
2022-12-01 12:31:40 +0000

    REFACTOR: js like haskell

--- 2022/day-1/day1.js
+++ 2022/day-1/day1.js
@@ -1,22 +1,21 @@
 const fs = require("fs")
+const os = require("os")
 
 // parse
 nums = fs.readFileSync(process.argv[2], "utf-8")
-    .split("\n")
-    .map((x) => parseInt(x))
+    .split(os.EOL + os.EOL)
+    .map((x) =>
+        x.split(os.EOL)
+            .map((y) => parseInt(y))
+            .filter((v) => !isNaN(v))
+            .reduce((a, v) => a + v)
+
+    )
+    .sort((a, b) => b - a)
 
 // part1
-arr = [0]
-for (i = 0; i < nums.length; i++) {
-    if (isNaN(nums[i])) {
-        arr.push(0)
-    } else {
-        arr[arr.length - 1] += nums[i]
-    }
-}
-console.log(arr.reduce((a, b) => Math.max(a, b), -Infinity))
+console.log(nums[0])
 
 // part2
-arr.sort((a, b) => b - a)
-top3 = (arr.slice(0, 3))
+top3 = (nums.slice(0, 3))
 console.log(top3.reduce((a, v) => a + v))
