f3fd40aa2928057e14a9f6b70d5baff420b88c87
Tom Ratford <tomratford@gmail.com>
2023-12-01 21:24:08 +0000

    simpler sol

--- 2023/day-1/part2.hs
+++ 2023/day-1/part2.hs
@@ -7,15 +7,15 @@ main = do
 
 convert :: String -> String
 convert [] = []
-convert ('o':'n':'e':a) = 'o':'1':convert ('e':a)
-convert ('t':'w':'o':a) = 't':'2':convert ('o':a)
-convert ('t':'h':'r':'e':'e':a) = 't':'3':convert ('e':a)
-convert ('f':'o':'u':'r':a) = 'f':'4':convert ('r':a)
-convert ('f':'i':'v':'e':a) = 'f':'5':convert ('e':a)
-convert ('s':'i':'x':a) = 's':'6':convert ('x':a)
-convert ('s':'e':'v':'e':'n':a) = 's':'7':convert ('n':a)
-convert ('e':'i':'g':'h':'t':a) = 'e':'8':convert ('t':a)
-convert ('n':'i':'n':'e':a) = 'n':'9':convert ('e':a)
+convert ('o':'n':'e':a) = '1':convert ('e':a)
+convert ('t':'w':'o':a) = '2':convert ('o':a)
+convert ('t':'h':'r':'e':'e':a) = '3':convert ('e':a)
+convert ('f':'o':'u':'r':a) = '4':convert ('r':a)
+convert ('f':'i':'v':'e':a) = '5':convert ('e':a)
+convert ('s':'i':'x':a) = '6':convert ('x':a)
+convert ('s':'e':'v':'e':'n':a) = '7':convert ('n':a)
+convert ('e':'i':'g':'h':'t':a) = '8':convert ('t':a)
+convert ('n':'i':'n':'e':a) = '9':convert ('e':a)
 convert (a:as) = a:convert as
 
 part1 :: String -> Int
