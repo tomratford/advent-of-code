aa9a2dbebb454ece10208a1b5d8eceebd5e49c70
Tom Ratford <tom.ratford@veramed.co.uk>
2024-11-04 09:30:24 +0000

    day-13 p2 - rough

--- 2023/day-13/main.go
+++ 2023/day-13/main.go
@@ -36,8 +36,8 @@ func main() {
 	//fmt.Println(part1_inner(p[2], n[2]))
 	fmt.Println(Part1(p, n))
 
-	fmt.Println(part2_inner(p[11], n[11]))
-	// fmt.Println(Part2(p, n))
+	//fmt.Println(part2_inner(p[87], n[87]))
+	fmt.Println(Part2(p, n))
 }
 
 // Structs and types
@@ -142,19 +142,19 @@ func Part2(input []string, line_lens []int) int {
 
 func part2_inner(input string, line_len int) int {
 	part1 := part1_inner(input, line_len)
-	// if vertical2 := findVertical2(rotate(rotate(input, line_len))); vertical2 != -1 && vertical2 != part1 {
-	// 	// fmt.Println(vertical2)
-	// 	number_of_lines := len(input) / line_len
-	// 	return (number_of_lines - vertical2) * 100
-	// }
-	// if vertical := findVertical2(input, line_len); vertical != -1 && vertical != part1 {
-	// 	// fmt.Println(vertical)
-	// 	return vertical * 100
-	// }
-	// if horizontal := findVertical2(rotate(input, line_len)); horizontal != -1 && horizontal != part1 {
-	// 	// fmt.Println(horizontal)
-	// 	return line_len - horizontal
-	// }
+	if vertical2 := findVertical2(rotate(rotate(input, line_len))); vertical2 != -1 && vertical2 != part1 {
+		// fmt.Println(vertical2)
+		number_of_lines := len(input) / line_len
+		return (number_of_lines - vertical2) * 100
+	}
+	if vertical := findVertical2(input, line_len); vertical != -1 && vertical != part1 {
+		// fmt.Println(vertical)
+		return vertical * 100
+	}
+	if horizontal := findVertical2(rotate(input, line_len)); horizontal != -1 && horizontal != part1 {
+		// fmt.Println(horizontal)
+		return line_len - horizontal
+	}
 	if horizontal2 := findVertical2(rotate(rotate(rotate(input, line_len)))); horizontal2 != -1 && horizontal2 != part1 {
 		// fmt.Println(horizontal2)
 		return horizontal2
@@ -163,21 +163,27 @@ func part2_inner(input string, line_len int) int {
 	return 0
 }
 
-// Finds a vertical reflection across a horizontal line
+/*
+Finds a vertical reflection across a horizontal line
+
+n the line length
+*/
 func findVertical2(input string, n int) int {
 	i := 0
 	j := len(input)
-	m := n
+	m := n // How much to include from the line
+	n_lines := len(input) / n
 	for {
 		if i+m > len(input) || j-m < 0 {
-			if i/n < len(input)/n {
-				m = n
-				i += n
+			if curr_line := i / n; curr_line < n_lines {
+				m = n  // Reset back for next line
+				i += n // go to next line
 				continue
 			}
 			return -1
 		}
 		left := input[i : i+m]
+		// Have to write right hand side such that it is added in reverse order
 		var rb strings.Builder
 		for k := 1; k <= m/n; k++ {
 			if j-m < 0 {
@@ -186,27 +192,38 @@ func findVertical2(input string, n int) int {
 			rb.WriteString(input[j-k*n : j-((k-1)*n)])
 		}
 		right := rb.String()
-		fmt.Println(left, right, distance(left, right), ((i/n)+(j/n))/2, i+2*n, j-m-n)
+		// if dist := distance(left, right); dist == 1 {
+		// 	printDiff(left, right)
+		// 	fmt.Println(right, distance(left, right), i+m, j-m, i, j, m)
+		// }
 		if dist := distance(left, right); dist > 1 {
-			if dist == 2 && i+2*n == j-m-n { // edge case
-				return ((i / n) + (j / n)) / 2 // midpoint
-			}
 			m = n
 			i += n
 		}
 		if dist := distance(left, right); dist <= 1 {
-			if j-m-i-n == 2*n && dist == 1 {
-				return ((i / n) + (j / n)) / 2 // midpoint
-			}
-			if i+n == j-m && dist == 1 {
+			// if j-m-i-n == 2*n && dist == 1 {
+			// 	return ((i / n) + (j / n)) / 2 // midpoint
+			// }
+			if i+m == j-m && dist == 1 {
 				return ((i / n) + (j / n)) / 2 // midpoint
 			}
 			m += n
-			if i+n == j-m && dist == 1 {
-				return ((i / n) + (j / n)) / 2 // midpoint
-			}
+			// if i+n == j-m && dist == 1 {
+			// 	return ((i / n) + (j / n)) / 2 // midpoint
+			// }
 		}
 	}
+}
+
+func printDiff(left, right string) {
+	for i := range left {
+		if left[i] == right[i] {
+			fmt.Print(string(left[i]))
+		} else {
+			fmt.Printf("\033[31m%s\033[0m", string(left[i]))
+		}
+	}
+	fmt.Print("\n")
 }
 
 func distance(x, y string) int {
