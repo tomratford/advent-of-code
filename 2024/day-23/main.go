720751f93aea6e37c5971176c27ef5246f420935
Tom Ratford <tom.ratford@veramed.co.uk>
2024-12-23 09:06:36 +0000

    day23 p1

--- 2024/day-23/main.go
+++ 2024/day-23/main.go
@@ -39,40 +39,36 @@ func main() {
 
 // Structs and types
 
-type Set struct {
-	data map[string]int
+type Set[T comparable] struct {
+	data map[T]int
 }
 
-func (s *Set) Len() int {
+func (s *Set[T]) Len() int {
 	return len(s.data)
 }
 
-func (s *Set) String() string {
-	rtn := make([]string, 0, len(s.data))
-	for v := range s.data {
-		rtn = append(rtn, v)
-	}
-	return fmt.Sprintf("%v", rtn)
+func (s *Set[T]) String() string {
+	return fmt.Sprintf("%v", s.Values())
 }
 
-func NewSet(data ...string) *Set {
-	m := make(map[string]int)
+func NewSet[T comparable](data ...T) *Set[T] {
+	m := make(map[T]int)
 	for _, s := range data {
 		m[s]++
 	}
-	return &Set{
+	return &Set[T]{
 		data: m,
 	}
 }
 
-func (s *Set) Add(data ...string) {
+func (s *Set[T]) Add(data ...T) {
 	for _, v := range data {
 		s.data[v]++
 	}
 }
 
-func Intersect(s1, s2 *Set) *Set {
-	s := NewSet()
+func Intersect[T comparable](s1, s2 *Set[T]) *Set[T] {
+	s := NewSet[T]()
 	for k1 := range s1.data {
 		for k2 := range s2.data {
 			if k1 == k2 {
@@ -83,105 +79,49 @@ func Intersect(s1, s2 *Set) *Set {
 	return s
 }
 
-func (s *Set) Values() []string {
-	rtn := make([]string, 0, len(s.data))
+func (s *Set[T]) Values() []T {
+	rtn := make([]T, 0, len(s.data))
 	for k := range s.data {
 		rtn = append(rtn, k)
 	}
 	return rtn
 }
 
-func (s *Set) ContainsFunc(f func(s string) bool) bool {
+func (s *Set[T]) ContainsFunc(f func(V T) bool) bool {
 	return slices.ContainsFunc(s.Values(), f)
-}
-
-func (s *Set) CountFunc(f func(s string) bool) int {
-	rtn := 0
-	values := s.Values()
-	for _, v := range values {
-		if f(v) {
-			rtn++
-		}
-	}
-	return rtn
 }
 
 // Solution for Part 1 of the challenge
 func Part1(input map[string][]string) int {
-	seen := make(map[string]int)
 	rtn := 0
+	values := NewSet[[3]string]()
 	for k, v := range input {
-		s := NewSet(k)
-		s.Add(v...)
-	outer:
-		for i := 0; i < len(v); i++ {
-			for j := 0; j < len(v); j++ {
-				if _, ok := seen[v[i]]; ok {
-					continue outer
+		for _, a := range v {
+			if a == k {
+				continue
+			}
+			for _, b := range input[a] {
+				if b == k || b == a {
+					continue
 				}
-				if _, ok := seen[v[j]]; ok {
-					continue outer
-				}
-				si := NewSet(v[i])
-				si.Add(input[v[i]]...)
-				sj := NewSet(v[j])
-				sj.Add(input[v[j]]...)
-				overlap := Intersect(s, si)
-				overlap = Intersect(overlap, sj)
-				values := overlap.Values()
-				slices.Sort(values)
-				key := strings.Join(values, "")
-				if _, ok := seen[key]; ok {
-					continue outer
-				}
-				if overlap.Len() >= 3 {
-					//fmt.Println(overlap, key)
-					seen[key]++
-					if t_count := overlap.CountFunc(func(s string) bool {
-						return strings.Contains(s, "t")
-					}); t_count > 0 {
-						fmt.Println(overlap, t_count, getScore(t_count, overlap.Len()))
-						rtn += getScore(t_count, overlap.Len())
-					}
-					break outer
+				if slices.Contains(input[b], k) {
+					tmp := []string{k, a, b}
+					slices.Sort(tmp) // Ensure we don't add multiple versions
+					var store [3]string
+					copy(store[:], tmp)
+					values.Add(store)
 				}
 			}
 		}
 	}
+	fmt.Println(values)
+	for _, s := range values.Values() {
+		sl := s[:]
+		if slices.ContainsFunc(sl, func(s string) bool { return strings.HasPrefix(s, "t") }) {
+			rtn++
+		}
+	}
 	return rtn
-}
-
-// Work out the number of combinations when we have potentially more than one t
-func getScore(t_count, length int) int {
-	if t_count == 0 {
-		return 0
-	}
-
-	c := length - t_count
-	if c > 2 {
-		c = 2
-	}
-	return combination(length-1, 2) + getScore(t_count-1, length-1)
-}
-
-// Factorial function to calculate n!
-func factorial(n int) int {
-	if n == 0 {
-		return 1
-	}
-	result := 1
-	for i := 1; i <= n; i++ {
-		result *= i
-	}
-	return result
-}
-
-// Combination function to calculate n Choose R
-func combination(n, r int) int {
-	if r > n {
-		return 0 // If r > n, the result is 0 (invalid combination)
-	}
-	return factorial(n) / (factorial(r) * factorial(n-r))
 }
 
 // Solution for Part 2 of the challenge
